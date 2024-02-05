cdfb1e5d9ce10d86a0708579c8c4757394f558ea
dilne <155658788+danmilneusw@users.noreply.github.com>
2024-02-05 13:24:47 +0000

    Create Unity Project - Reusing Objects

---
+++ Unity Project - Reusing Objects/Assets/Scripts/Game.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : PersistableObject {
+
+	const int saveVersion = 1;
+
+	public ShapeFactory shapeFactory;
+
+	public KeyCode createKey = KeyCode.C;
+	public KeyCode destroyKey = KeyCode.X;
+	public KeyCode newGameKey = KeyCode.N;
+	public KeyCode saveKey = KeyCode.S;
+	public KeyCode loadKey = KeyCode.L;
+
+	public PersistentStorage storage;
+	public float CreationSpeed { get; set; }
+	public float DestructionSpeed { get; set; }
+	float creationProgress, destructionProgress;
+
+	List<Shape> shapes;
+
+	void Awake () {
+		shapes = new List<Shape>();
+	}
+
+	void Update () {
+		if (Input.GetKeyDown(createKey)) {
+			CreateShape();
+		}
+		else if (Input.GetKeyDown(destroyKey)) {
+			DestroyShape();
+		}
+		else if (Input.GetKeyDown(newGameKey)) {
+			BeginNewGame();
+		}
+		else if (Input.GetKeyDown(saveKey)) {
+			storage.Save(this, saveVersion);
+		}
+		else if (Input.GetKeyDown(loadKey)) {
+			BeginNewGame();
+			storage.Load(this);
+		}
+
+		creationProgress += Time.deltaTime * CreationSpeed;
+		while (creationProgress >= 1f) {
+			creationProgress -= 1f;
+			CreateShape();
+		}
+
+		destructionProgress += Time.deltaTime * DestructionSpeed;
+		while (destructionProgress >= 1f) {
+			destructionProgress -= 1f;
+			DestroyShape();
+		}
+	}
+
+	void BeginNewGame () {
+		for (int i = 0; i < shapes.Count; i++) {
+			Destroy(shapes[i].gameObject);
+		}
+		shapes.Clear();
+	}
+
+	void CreateShape () {
+		Shape instance = shapeFactory.GetRandom();
+		Transform t = instance.transform;
+		t.localPosition = Random.insideUnitSphere * 5f;
+		t.localRotation = Random.rotation;
+		t.localScale = Vector3.one * Random.Range(0.1f, 1f);
+		instance.SetColor(Random.ColorHSV(
+			hueMin: 0f, hueMax: 1f,
+			saturationMin: 0.5f, saturationMax: 1f,
+			valueMin: 0.25f, valueMax: 1f,
+			alphaMin: 1f, alphaMax: 1f
+		));
+		shapes.Add(instance);
+	}
+
+	void DestroyShape () {
+		if (shapes.Count > 0) {
+			int index = Random.Range(0, shapes.Count);
+			Destroy(shapes[index].gameObject);
+			int lastIndex = shapes.Count - 1;
+			shapes[index] = shapes[lastIndex];
+			shapes.RemoveAt(lastIndex);
+		}
+	}
+
+	public override void Save (GameDataWriter writer) {
+		writer.Write(shapes.Count);
+		for (int i = 0; i < shapes.Count; i++) {
+			writer.Write(shapes[i].ShapeId);
+			writer.Write(shapes[i].MaterialId);
+			shapes[i].Save(writer);
+		}
+	}
+
+	public override void Load (GameDataReader reader) {
+		int version = reader.Version;
+		if (version > saveVersion) {
+			Debug.LogError("Unsupported future save version " + version);
+			return;
+		}
+		int count = version <= 0 ? -version : reader.ReadInt();
+		for (int i = 0; i < count; i++) {
+			int shapeId = version > 0 ? reader.ReadInt() : 0;
+			int materialId = version > 0 ? reader.ReadInt() : 0;
+			Shape instance = shapeFactory.Get(shapeId, materialId);
+			instance.Load(reader);
+			shapes.Add(instance);
+		}
+	}
+}
---
+++ Unity Project - Reusing Objects/README.md
@@ -0,0 +1,11 @@
+# Object Management 02: Object Variety
+
+[This is the second tutorial in a series about Object Management.](https://catlikecoding.com/unity/tutorials/object-management/object-variety/) In this part we'll add support for multiple shapes with varying materials and colors, while remaining backwards compatible with the previous version of our game.
+
+## License
+
+You can do whatever you want with the files in this project. I offer neither guarantees nor warranties. If you use the files, then you're liable for them. It would be nice if you gave me credit.
+
+## Author
+
+[Jasper Flick](https://catlikecoding.com/jasper-flick/)
